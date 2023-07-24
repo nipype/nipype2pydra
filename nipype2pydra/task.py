3d29e9ac3e15dda86c6ff91eadba3940d516e369
yibeichan <yibeichan@gmail.com>
2023-07-24 10:49:27 -0400

    fix the converting problem by adding prefix

--- nipype2pydra/task.py
+++ nipype2pydra/task.py
@@ -21,7 +21,7 @@ class TaskConverter:
     nipype_module: ModuleType = attrs.field(converter=import_module_from_path)
     output_requirements: dict = attrs.field(factory=dict)
     inputs_metadata: dict = attrs.field(factory=dict)
-    inputs_drop: dict = attrs.field(factory=list)
+    inputs_drop: dict = attrs.field(factory=dict)
     output_templates: dict = attrs.field(factory=dict)
     output_callables: dict = attrs.field(factory=dict)
     doctest: dict = attrs.field(factory=dict)
@@ -305,8 +305,13 @@ class TaskConverter:
                 el = list(el)
                 try:
                     el[1] = el[1].__name__
+                    # add 'TYPE_' to the beginning of the name
+                    el[1] = "TYPE_" + el[1]
                 except (AttributeError):
                     el[1] = el[1]._name
+                    # add 'TYPE_' to the beginning of the name
+                    el[1] = "TYPE_" + el[1]
+                print(tuple(el))
                 spec_fields_str.append(tuple(el))
             return spec_fields_str
 
@@ -330,17 +335,8 @@ class TaskConverter:
         spec_str += f"    output_spec = {self.task_name}_output_spec\n"
         spec_str += f"    executable='{self.nipype_interface._cmd}'\n"
 
-        # for tp_repl in self.TYPE_REPLACE:
-        #     spec_str = spec_str.replace(*tp_repl)
-
-        # apply each replacement in TYPE_REPLACE
-        for old, new in self.TYPE_REPLACE:
-            # add a comma and newline to the old string, to match only at the end of lines
-            old += ",\n"
-            # add a comma and newline to the new string, to replace with
-            new += ",\n"
-            # use re.sub to replace old with new, in spec_str
-            spec_str = re.sub(old, new, spec_str)
+        for tp_repl in self.TYPE_REPLACE:
+            spec_str = spec_str.replace(*tp_repl)
 
         spec_str_black = black.format_file_contents(
             spec_str, fast=False, mode=black.FileMode()
@@ -464,16 +460,16 @@ class TaskConverter:
     ]
 
     TYPE_REPLACE = [
-        ("'File'", "specs.File"),
-        ("'bool'", "bool"),
-        ("'str'", "str"),
-        ("'Any'", "ty.Any"),
-        ("'int'", "int"),
-        ("'float'", "float"),
-        ("'list'", "list"),
-        ("'dict'", "dict"),
-        ("'MultiInputObj'", "specs.MultiInputObj"),
-        ("'MultiOutputObj'", "specs.MultiOutputObj"),
-        ("'MultiInputFile'", "specs.MultiInputFile"),
-        ("'MultiOutputFile'", "specs.MultiOutputFile"),
+        ("'TYPE_File'", "specs.File"),
+        ("'TYPE_bool'", "bool"),
+        ("'TYPE_str'", "str"),
+        ("'TYPE_Any'", "ty.Any"),
+        ("'TYPE_int'", "int"),
+        ("'TYPE_float'", "float"),
+        ("'TYPE_list'", "list"),
+        ("'TYPE_dict'", "dict"),
+        ("'TYPE_MultiInputObj'", "specs.MultiInputObj"),
+        ("'TYPE_MultiOutputObj'", "specs.MultiOutputObj"),
+        ("'TYPE_MultiInputFile'", "specs.MultiInputFile"),
+        ("'TYPE_MultiOutputFile'", "specs.MultiOutputFile"),
     ]
