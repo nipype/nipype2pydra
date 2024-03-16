0694d7b9e733063a55f73a43a06810b4ed01ff92
Tom Close <tom.g.close@gmail.com>
2024-03-16 18:20:35 +1100

    stripped out examples dir

--- nipype2pydra/tests/test_pkg_gen.py
+++ nipype2pydra/tests/test_pkg_gen.py
@@ -4,9 +4,7 @@ from conftest import show_cli_trace
 
 def test_pkg_gen(cli_runner, tmp_path):
     outputs_dir = tmp_path / "output-dir"
-    examples_dir = tmp_path / "export-dir"
     outputs_dir.mkdir()
-    examples_dir.mkdir()
 
     result = cli_runner(
         pkg_gen,
@@ -14,8 +12,6 @@ def test_pkg_gen(cli_runner, tmp_path):
             str(outputs_dir),
             "--work-dir",
             str(tmp_path / "work-dir"),
-            "--example-packages",
-            str(examples_dir),
         ],
     )
     assert result.exit_code == 0, show_cli_trace(result)
