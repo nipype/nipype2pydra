95413b063b5ba5dd285792c1b1be94d92291f163
Tom Close <tom.g.close@gmail.com>
2024-03-27 16:11:07 +1100

    implemented and tested split statements util

--- nipype2pydra/tests/test_utils.py
+++ nipype2pydra/tests/test_utils.py
@@ -1,4 +1,8 @@
-from nipype2pydra.utils import extract_args, get_source_code
+from nipype2pydra.utils import (
+    extract_args,
+    get_source_code,
+    split_source_into_statements,
+)
 from nipype2pydra.testing import test_line_number_of_function
 
 
@@ -89,3 +93,300 @@ def test_source_code():
         "# Original source at L1 of <nipype2pydra-install>/testing.py",
         "def test_line_number_of_function():",
     ]
+
+    # \"\"\"
+    # One-subject-one-session-one-run pipeline to extract the NR-IQMs from
+    # anatomical images
+
+    # .. workflow::
+
+    #     import os.path as op
+    #     from mriqc.workflows.anatomical.base import anat_qc_workflow
+    #     from mriqc.testing import mock_config
+    #     with mock_config():
+    #         wf = anat_qc_workflow()
+
+    # \"\"\"
+
+
+EXAMPLE_SOURCE_CODE = """
+    from mriqc.workflows.shared import synthstrip_wf
+
+    dataset = config.workflow.inputs.get('t1w', []) + config.workflow.inputs.get('t2w', [])
+
+    message = BUILDING_WORKFLOW.format(
+        modality='anatomical',
+        detail=(
+            f'for {len(dataset)} NIfTI files.'
+            if len(dataset) > 2
+            else f"({' and '.join('<%s>' % v for v in dataset)})."
+        ),
+    )
+    config.loggers.workflow.info(message)
+
+    # Initialize workflow
+    workflow = pe.Workflow(name=name)
+
+    # Define workflow, inputs and outputs
+    # 0. Get data
+    inputnode = pe.Node(niu.IdentityInterface(fields=['in_file']), name='inputnode')
+    inputnode.iterables = [('in_file', dataset)]
+
+    datalad_get = pe.Node(
+        DataladIdentityInterface(fields=['in_file'], dataset_path=config.execution.bids_dir),
+        name='datalad_get',
+    )
+
+    outputnode = pe.Node(niu.IdentityInterface(fields=['out_json']), name='outputnode')
+
+    # 1. Reorient anatomical image
+    to_ras = pe.Node(ConformImage(check_dtype=False), name='conform')
+    # 2. species specific skull-stripping
+    if config.workflow.species.lower() == 'human':
+        skull_stripping = synthstrip_wf(omp_nthreads=config.nipype.omp_nthreads)
+        ss_bias_field = 'outputnode.bias_image'
+    else:
+        from nirodents.workflows.brainextraction import init_rodent_brain_extraction_wf
+
+        skull_stripping = init_rodent_brain_extraction_wf(template_id=config.workflow.template_id)
+        ss_bias_field = 'final_n4.bias_image'
+    # 3. Head mask
+    hmsk = headmsk_wf(omp_nthreads=config.nipype.omp_nthreads)
+    # 4. Spatial Normalization, using ANTs
+    norm = spatial_normalization()
+    # 5. Air mask (with and without artifacts)
+    amw = airmsk_wf()
+    # 6. Brain tissue segmentation
+    bts = init_brain_tissue_segmentation()
+    # 7. Compute IQMs
+    iqmswf = compute_iqms()
+    # Reports
+    anat_report_wf = init_anat_report_wf()
+
+    # Connect all nodes
+    # fmt: off
+    workflow.connect([
+        (inputnode, datalad_get, [('in_file', 'in_file')]),
+        (inputnode, anat_report_wf, [
+            ('in_file', 'inputnode.name_source'),
+        ]),
+        (datalad_get, to_ras, [('in_file', 'in_file')]),
+        (datalad_get, iqmswf, [('in_file', 'inputnode.in_file')]),
+        (datalad_get, norm, [(('in_file', _get_mod), 'inputnode.modality')]),
+        (to_ras, skull_stripping, [('out_file', 'inputnode.in_files')]),
+        (skull_stripping, hmsk, [
+            ('outputnode.out_corrected', 'inputnode.in_file'),
+            ('outputnode.out_mask', 'inputnode.brainmask'),
+        ]),
+        (skull_stripping, bts, [('outputnode.out_mask', 'inputnode.brainmask')]),
+        (skull_stripping, norm, [
+            ('outputnode.out_corrected', 'inputnode.moving_image'),
+            ('outputnode.out_mask', 'inputnode.moving_mask')]),
+        (norm, bts, [('outputnode.out_tpms', 'inputnode.std_tpms')]),
+        (norm, amw, [
+            ('outputnode.ind2std_xfm', 'inputnode.ind2std_xfm')]),
+        (norm, iqmswf, [
+            ('outputnode.out_tpms', 'inputnode.std_tpms')]),
+        (norm, anat_report_wf, ([
+            ('outputnode.out_report', 'inputnode.mni_report')])),
+        (norm, hmsk, [('outputnode.out_tpms', 'inputnode.in_tpms')]),
+        (to_ras, amw, [('out_file', 'inputnode.in_file')]),
+        (skull_stripping, amw, [('outputnode.out_mask', 'inputnode.in_mask')]),
+        (hmsk, amw, [('outputnode.out_file', 'inputnode.head_mask')]),
+        (to_ras, iqmswf, [('out_file', 'inputnode.in_ras')]),
+        (skull_stripping, iqmswf, [('outputnode.out_corrected', 'inputnode.inu_corrected'),
+                                   (ss_bias_field, 'inputnode.in_inu'),
+                                   ('outputnode.out_mask', 'inputnode.brainmask')]),
+        (amw, iqmswf, [('outputnode.air_mask', 'inputnode.airmask'),
+                       ('outputnode.hat_mask', 'inputnode.hatmask'),
+                       ('outputnode.art_mask', 'inputnode.artmask'),
+                       ('outputnode.rot_mask', 'inputnode.rotmask')]),
+        (hmsk, bts, [('outputnode.out_denoised', 'inputnode.in_file')]),
+        (bts, iqmswf, [('outputnode.out_segm', 'inputnode.segmentation'),
+                       ('outputnode.out_pvms', 'inputnode.pvms')]),
+        (hmsk, iqmswf, [('outputnode.out_file', 'inputnode.headmask')]),
+        (to_ras, anat_report_wf, [('out_file', 'inputnode.in_ras')]),
+        (skull_stripping, anat_report_wf, [
+            ('outputnode.out_corrected', 'inputnode.inu_corrected'),
+            ('outputnode.out_mask', 'inputnode.brainmask')]),
+        (hmsk, anat_report_wf, [('outputnode.out_file', 'inputnode.headmask')]),
+        (amw, anat_report_wf, [
+            ('outputnode.air_mask', 'inputnode.airmask'),
+            ('outputnode.art_mask', 'inputnode.artmask'),
+            ('outputnode.rot_mask', 'inputnode.rotmask'),
+        ]),
+        (bts, anat_report_wf, [('outputnode.out_segm', 'inputnode.segmentation')]),
+        (iqmswf, anat_report_wf, [('outputnode.noisefit', 'inputnode.noisefit')]),
+        (iqmswf, anat_report_wf, [('outputnode.out_file', 'inputnode.in_iqms')]),
+        (iqmswf, outputnode, [('outputnode.out_file', 'out_json')]),
+    ])
+    # fmt: on
+
+    # Upload metrics
+    if not config.execution.no_sub:
+        from mriqc.interfaces.webapi import UploadIQMs
+
+        upldwf = pe.Node(
+            UploadIQMs(
+                endpoint=config.execution.webapi_url,
+                auth_token=config.execution.webapi_token,
+                strict=config.execution.upload_strict,
+            ),
+            name='UploadMetrics',
+        )
+
+        # fmt: off
+        workflow.connect([
+            (iqmswf, upldwf, [('outputnode.out_file', 'in_iqms')]),
+            (upldwf, anat_report_wf, [('api_id', 'inputnode.api_id')]),
+        ])
+
+    # fmt: on
+
+    return workflow
+"""
+
+
+EXAMPLE_SOURCE_CODE_SPLIT = [
+    # """    \"\"\"
+    # One-subject-one-session-one-run pipeline to extract the NR-IQMs from
+    # anatomical images
+    # .. workflow::
+    #     import os.path as op
+    #     from mriqc.workflows.anatomical.base import anat_qc_workflow
+    #     from mriqc.testing import mock_config
+    #     with mock_config():
+    #         wf = anat_qc_workflow()
+    # \"\"\"""",
+    "",
+    "    from mriqc.workflows.shared import synthstrip_wf",
+    "",
+    "    dataset = config.workflow.inputs.get('t1w', []) + config.workflow.inputs.get('t2w', [])",
+    "",
+    """    message = BUILDING_WORKFLOW.format(modality='anatomical', detail=(
+            f'for {len(dataset)} NIfTI files.'
+            if len(dataset) > 2
+            else f"({' and '.join('<%s>' % v for v in dataset)})."
+        ))""",
+    "    config.loggers.workflow.info(message)",
+    "",
+    "    # Initialize workflow",
+    "    workflow = pe.Workflow(name=name)",
+    "",
+    "    # Define workflow, inputs and outputs",
+    "    # 0. Get data",
+    "    inputnode = pe.Node(niu.IdentityInterface(fields=['in_file']), name='inputnode')",
+    "    inputnode.iterables = [('in_file', dataset)]",
+    "",
+    """    datalad_get = pe.Node(DataladIdentityInterface(fields=['in_file'], dataset_path=config.execution.bids_dir), name='datalad_get')""",
+    "",
+    "    outputnode = pe.Node(niu.IdentityInterface(fields=['out_json']), name='outputnode')",
+    "",
+    "    # 1. Reorient anatomical image",
+    "    to_ras = pe.Node(ConformImage(check_dtype=False), name='conform')",
+    "    # 2. species specific skull-stripping",
+    "    if config.workflow.species.lower() == 'human':",
+    "        skull_stripping = synthstrip_wf(omp_nthreads=config.nipype.omp_nthreads)",
+    "        ss_bias_field = 'outputnode.bias_image'",
+    "    else:",
+    "        from nirodents.workflows.brainextraction import init_rodent_brain_extraction_wf",
+    "",
+    "        skull_stripping = init_rodent_brain_extraction_wf(template_id=config.workflow.template_id)",
+    "        ss_bias_field = 'final_n4.bias_image'",
+    "    # 3. Head mask",
+    "    hmsk = headmsk_wf(omp_nthreads=config.nipype.omp_nthreads)",
+    "    # 4. Spatial Normalization, using ANTs",
+    "    norm = spatial_normalization()",
+    "    # 5. Air mask (with and without artifacts)",
+    "    amw = airmsk_wf()",
+    "    # 6. Brain tissue segmentation",
+    "    bts = init_brain_tissue_segmentation()",
+    "    # 7. Compute IQMs",
+    "    iqmswf = compute_iqms()",
+    "    # Reports",
+    "    anat_report_wf = init_anat_report_wf()",
+    "",
+    "    # Connect all nodes",
+    "    # fmt: off",
+    """    workflow.connect([
+        (inputnode, datalad_get, [('in_file', 'in_file')]),
+        (inputnode, anat_report_wf, [
+            ('in_file', 'inputnode.name_source'),
+        ]),
+        (datalad_get, to_ras, [('in_file', 'in_file')]),
+        (datalad_get, iqmswf, [('in_file', 'inputnode.in_file')]),
+        (datalad_get, norm, [(('in_file', _get_mod), 'inputnode.modality')]),
+        (to_ras, skull_stripping, [('out_file', 'inputnode.in_files')]),
+        (skull_stripping, hmsk, [
+            ('outputnode.out_corrected', 'inputnode.in_file'),
+            ('outputnode.out_mask', 'inputnode.brainmask'),
+        ]),
+        (skull_stripping, bts, [('outputnode.out_mask', 'inputnode.brainmask')]),
+        (skull_stripping, norm, [
+            ('outputnode.out_corrected', 'inputnode.moving_image'),
+            ('outputnode.out_mask', 'inputnode.moving_mask')]),
+        (norm, bts, [('outputnode.out_tpms', 'inputnode.std_tpms')]),
+        (norm, amw, [
+            ('outputnode.ind2std_xfm', 'inputnode.ind2std_xfm')]),
+        (norm, iqmswf, [
+            ('outputnode.out_tpms', 'inputnode.std_tpms')]),
+        (norm, anat_report_wf, ([
+            ('outputnode.out_report', 'inputnode.mni_report')])),
+        (norm, hmsk, [('outputnode.out_tpms', 'inputnode.in_tpms')]),
+        (to_ras, amw, [('out_file', 'inputnode.in_file')]),
+        (skull_stripping, amw, [('outputnode.out_mask', 'inputnode.in_mask')]),
+        (hmsk, amw, [('outputnode.out_file', 'inputnode.head_mask')]),
+        (to_ras, iqmswf, [('out_file', 'inputnode.in_ras')]),
+        (skull_stripping, iqmswf, [('outputnode.out_corrected', 'inputnode.inu_corrected'),
+                                   (ss_bias_field, 'inputnode.in_inu'),
+                                   ('outputnode.out_mask', 'inputnode.brainmask')]),
+        (amw, iqmswf, [('outputnode.air_mask', 'inputnode.airmask'),
+                       ('outputnode.hat_mask', 'inputnode.hatmask'),
+                       ('outputnode.art_mask', 'inputnode.artmask'),
+                       ('outputnode.rot_mask', 'inputnode.rotmask')]),
+        (hmsk, bts, [('outputnode.out_denoised', 'inputnode.in_file')]),
+        (bts, iqmswf, [('outputnode.out_segm', 'inputnode.segmentation'),
+                       ('outputnode.out_pvms', 'inputnode.pvms')]),
+        (hmsk, iqmswf, [('outputnode.out_file', 'inputnode.headmask')]),
+        (to_ras, anat_report_wf, [('out_file', 'inputnode.in_ras')]),
+        (skull_stripping, anat_report_wf, [
+            ('outputnode.out_corrected', 'inputnode.inu_corrected'),
+            ('outputnode.out_mask', 'inputnode.brainmask')]),
+        (hmsk, anat_report_wf, [('outputnode.out_file', 'inputnode.headmask')]),
+        (amw, anat_report_wf, [
+            ('outputnode.air_mask', 'inputnode.airmask'),
+            ('outputnode.art_mask', 'inputnode.artmask'),
+            ('outputnode.rot_mask', 'inputnode.rotmask'),
+        ]),
+        (bts, anat_report_wf, [('outputnode.out_segm', 'inputnode.segmentation')]),
+        (iqmswf, anat_report_wf, [('outputnode.noisefit', 'inputnode.noisefit')]),
+        (iqmswf, anat_report_wf, [('outputnode.out_file', 'inputnode.in_iqms')]),
+        (iqmswf, outputnode, [('outputnode.out_file', 'out_json')]),
+    ])""",
+    "    # fmt: on",
+    "",
+    "    # Upload metrics",
+    "    if not config.execution.no_sub:",
+    "        from mriqc.interfaces.webapi import UploadIQMs",
+    "",
+    """        upldwf = pe.Node(UploadIQMs(
+                endpoint=config.execution.webapi_url,
+                auth_token=config.execution.webapi_token,
+                strict=config.execution.upload_strict,
+            ), name='UploadMetrics')""",
+    "",
+    "        # fmt: off",
+    """        workflow.connect([
+            (iqmswf, upldwf, [('outputnode.out_file', 'in_iqms')]),
+            (upldwf, anat_report_wf, [('api_id', 'inputnode.api_id')]),
+        ])""",
+    "",
+    "    # fmt: on",
+    "",
+    "    return workflow",
+]
+
+
+def test_split_into_statements():
+    stmts = split_source_into_statements(EXAMPLE_SOURCE_CODE)
+    assert stmts == EXAMPLE_SOURCE_CODE_SPLIT
--- nipype2pydra/workflow.py
+++ nipype2pydra/workflow.py
@@ -1,9 +1,12 @@
 from importlib import import_module
 from functools import cached_property
 import inspect
+import re
+import typing as ty
 from types import ModuleType
 from pathlib import Path
 import attrs
+from .utils import UsedSymbols, split_source_into_statements, extract_args
 
 
 @attrs.define
@@ -21,9 +24,23 @@ class WorkflowConverter:
         the nipype module or module path containing the Nipype interface
     output_module: str
         the output module to store the converted task into relative to the `pydra.tasks` package
-    input_struct: tuple[str, type]
+    input_struct: tuple[str, str], optional
         a globally accessible structure containing inputs to the workflow, e.g. config.workflow.*
         tuple consists of the name of the input and the type of the input
+    inputnode : str, optional
+        the name of the workflow's input node (to be mapped to lzin), by default 'inputnode'
+    outputnode : str, optional
+        the name of the workflow's output node (to be mapped to lzout), by default 'outputnode'
+    potential_nested_workflows : list[str]
+        The specs of potentially nested workflows functions that may be called within
+        the workflow function
+    omit_interfaces : list[str]
+        the list of interfaces to be omitted from the workflow (e.g. DataGrabber)
+    package_mappings : dict[str, str]
+        packages that should be mapped to a new location (typically Nipype based deps
+        such as niworkflows)
+    other_mappings: dict[str, str]
+        other name mappings between
     """
 
     name: str
@@ -35,25 +52,43 @@ class WorkflowConverter:
     input_struct: str = None
     inputnode: str = "inputnode"
     outputnode: str = "outputnode"
-    nested_workflow_funcs: list[str] = None
-    omit_nodes: list[str] = None
+    potential_nested_workflows: dict[str, dict] = attrs.field(factory=dict)
+    omit_interfaces: list[str] = attrs.field(factory=list)
+    package_mappings: dict[str, str] = attrs.field(factory=dict)
+    other_mappings: dict[str, str] = attrs.field(factory=dict)
+    workflow_variable: str = None
 
     @output_module.default
     def _output_module_default(self):
         return f"pydra.tasks.{self.nipype_module.__name__}"
+
+    @input_struct.validator
+    def input_struct_validator(self, _, value):
+        permitted = ("dict", "class")
+        if value[1] not in permitted:
+            raise ValueError(
+                "the second item in the input_struct arg names the type of structu and "
+                f"must be one of {permitted}"
+            )
 
     @cached_property
     def nipype_function(self):
         return getattr(self.nipype_module, self.nipype_name)
 
     @cached_property
-    def generate(self, package_root: Path):
+    def generate(self, package_root: Path) -> ty.List[str]:
         """Generate the Pydra task module
 
         Parameters
         ----------
         package_root: str
             the root directory of the package to write the module to
+
+        Returns
+        -------
+        UsedSymbols
+            symbols that are defined within the same package as the workflow function
+            that need to be converted too
         """
 
         output_module = package_root.joinpath(
@@ -61,9 +96,53 @@ class WorkflowConverter:
         ).with_suffix(".py")
         output_module.parent.mkdir(parents=True, exist_ok=True)
 
-        src = inspect.getsource(self.nipype_function)
+        func_src = inspect.getsource(self.nipype_function)
+
+        used = UsedSymbols.find(
+            self.nipype_module, [func_src], collapse_intra_pkg=False
+        )
+
+        for orig, new in self.other_mappings.items():
+            func_src = re.sub(r"\b" + orig + r"\b", new, func_src)
+
+        # Determine the name of the workflow variable if not provided
+        if self.workflow_variable is None:
+            returns = set(re.findall(r"^\s+return (\w+)", func_src, flags=re.MULTILINE))
+            if len(returns) > 1:
+                raise RuntimeError(f"Ambiguous return statements {returns}")
+            workflow_var = list(returns)[0]
+        else:
+            workflow_var = self.workflow_variable
+
+        preamble, args, post = extract_args(func_src)
+
+        postamble, body = func_src.split(post, 1)
+
+        if self.input_struct:
+            if self.input_struct[1] == "class":
+                input_struct_re = re.compile(r"\b" + self.input_struct + r"\.(\w+)\b")
+            elif self.input_struct[1] == "dict":
+                input_struct_re = re.compile(
+                    r"\b" + self.input_struct + r"\[(?:'|\")([^\]]+)(?:'|\")]"
+                )
+            else:
+                assert False
+            # Find all the inputs that have been used in the function
+            used_inputs = sorted(set(input_struct_re.findall(func_src)))
+            # Substitute the input struct with a variable of that name
+            func_src = input_struct_re.sub("\1", func_src)
+            # Insert the inputs that are used in the function body into the signature
+        else:
+            used_inputs = []
+
+        args += used_inputs
+
+        signature = preamble + ", ".join(args) + postamble
+        statements = split_source_into_statements(body)
 
         code_str = ""
 
         with open(output_module, "w") as f:
             f.write(code_str)
+
+        return used
