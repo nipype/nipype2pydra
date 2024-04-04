e4e6be7176b8854682f81f55775094ff046101c8
Tom Close <tom.g.close@gmail.com>
2024-04-04 12:38:36 +1100

    unused exception

--- nipype2pydra/exceptions.py
+++ nipype2pydra/exceptions.py
@@ -1,3 +1,6 @@
-
 class UnmatchedParensException(Exception):
-    pass
+    """Unmatched parentheses found in code snippet"""
+
+
+class UnmatchedQuoteException(Exception):
+    """Unmatched quote found in code snippet"""
